487fb83f10ca617ac71b16ff32b3c42c2e0fb7de
AllHateChris <carlos-christia@outlook.com>
2026-05-11 21:31:18 -0400

    ProjectService Atualizado

--- src/main/java/com/projectmanager/ProjectMannage/applicationservice/ProjectService.java
+++ src/main/java/com/projectmanager/ProjectMannage/applicationservice/ProjectService.java
@@ -6,30 +6,32 @@ import com.projectmanager.ProjectMannage.model.ProjectStatus;
 import com.projectmanager.ProjectMannage.repository.ProjectRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j; // Corrigido para o padrão do Lombok
 import org.springframework.stereotype.Service;
-
 
 @Service
 @RequiredArgsConstructor
+@Slf4j 
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
 
     @Transactional
-    public Project createProject (SaveProjectDataDTO saveProjectDataDTO) {
-    Project project = Project
-            .builder()
-            .name(saveProjectDataDTO.getName())
-            .description(saveProjectDataDTO.getDescription())
-            .initialDate(saveProjectDataDTO.getInitialDate())
-            .finalDate(saveProjectDataDTO.getFinalDate())
-            .status(ProjectStatus.PEDDING)
-            .build();
+    public Project createProject(SaveProjectDataDTO saveProjectDataDTO) {
+        Project project = Project
+                .builder()
+                .name(saveProjectDataDTO.getName())
+                .description(saveProjectDataDTO.getDescription())
+                .initialDate(saveProjectDataDTO.getInitialDate())
+                .finalDate(saveProjectDataDTO.getFinalDate())
+                .status(ProjectStatus.PEDDING)
+                .build();
 
         projectRepository.save(project);
+
+        // Usando a variável 'log' injetada pelo @Slf4j do Lombok
+        log.info("Project created: {}", project);
+
         return project;
     }
 }
-
-
-
